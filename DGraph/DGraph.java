45884123248c9a726d5513b16b8bee6a3c8f6af4
Avernad1 <leonk8867@gmail.com>
2022-05-27 14:57:32 -0400

    Depth First Search

---
+++ DGraph/DGraph.java
@@ -0,0 +1,48 @@
+package DGraph;
+import java.util.*;
+
+public class DGraph {
+    private LinkedList<Integer> adjlist[];
+    private boolean visited[];
+
+    DGraph(int vertices) {
+        adjlist = new LinkedList[vertices];
+        visited = new boolean[vertices];
+        for(int i=0; i<vertices; ++i) {
+            adjlist[i] = new LinkedList<Integer>();
+        }
+    }
+
+    void addEdge(int v, int w) {
+        adjlist[v].add(w);
+    }
+
+    void DFS(int vertex) {
+        visited[vertex] = true;
+        System.out.println(vertex + " ");
+        Iterator<Integer> i = adjlist[vertex].listIterator();
+        while(i.hasNext()) {
+            int adjlist = i.next();
+            if(!visited[adjlist]) {
+                DFS(adjlist);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        DGraph g = new DGraph(15);
+        g.addEdge(1,2);
+        g.addEdge(1,5);
+        g.addEdge(1,7);
+        g.addEdge(2,0);
+        g.addEdge(2, 8);
+        g.addEdge(8,6);
+        g.addEdge(6,12);
+        g.addEdge(6,4);
+
+        System.out.print("Depth First Search: ");
+        g.DFS(1);
+    }
+
+    
+}
