a0587983fd85a6f5a606bd3ac7e9ecb617a6aa4e
Avernad1 <leonk8867@gmail.com>
2022-05-27 08:45:04 -0400

    Breadth First Search

---
+++ BGraph/BGraph.java
@@ -0,0 +1,56 @@
+package BGraph;
+import java.util.*;
+
+public class BGraph {
+    private LinkedList adj[];
+    private int V;
+
+    BGraph(int v) {
+        V = v;
+        adj = new LinkedList[V];
+        for(int i=0; i<V; ++i) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void BFS(int s) {
+        boolean visited[] = new boolean[V];
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+
+        visited[s] = true;
+        queue.add(s);
+
+        while(queue.size() != 0) {
+            s = queue.poll();
+            System.out.print(s + " ");
+            Iterator<Integer> i = adj[s].listIterator();
+            while(i.hasNext()) {
+                int n = i.next();
+                if(!visited[n]) {
+                    visited[n]=true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        BGraph g = new BGraph(15);
+        g.addEdge(1,2);
+        g.addEdge(1,5);
+        g.addEdge(1,7);
+        g.addEdge(2,0);
+        g.addEdge(2, 8);
+        g.addEdge(8,6);
+        g.addEdge(6,12);
+        g.addEdge(6,4);
+
+        System.out.print("Breadth First Search: ");
+        g.BFS(1);
+    }
+    
+}
